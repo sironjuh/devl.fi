ca0817d85d27bbf9cffedb35e33ee0cd0f249e83
Juha-Matti Sironen <juha-matti.sironen@outlook.com>
2021-05-14 09:32:39 +0300

    [fresnel-glow] first commit

---
+++ fresnel-glow/app.js
@@ -0,0 +1,152 @@
+// Raymarching balls
+//
+// MDN WebGL tutorial used as a base for quick boilerplate
+// https://github.com/mdn/webgl-examples/blob/gh-pages/tutorial/sample2/webgl-demo.js
+//
+// Signed Distance Functions
+// https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+//
+// Inspiration
+// https://www.iquilezles.org/www/articles/raymarchingdf/raymarchingdf.htm
+
+async function main() {
+  const canvas = document.querySelector("#glcanvas");
+  const gl = canvas.getContext("webgl");
+
+  if (!gl) {
+    console.error("Unable to initialize WebGL");
+    return;
+  }
+
+  const vertexSource = await fetchShader("vertex.glsl");
+  const fragmentSource = await fetchShader("fragment.glsl");
+  const shaderProgram = initShaderProgram(gl, vertexSource, fragmentSource);
+
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, "a_vertex_pos"),
+    },
+    uniformLocations: {
+      resolution: gl.getUniformLocation(shaderProgram, "u_resolution"),
+      time: gl.getUniformLocation(shaderProgram, "u_time"),
+    },
+  };
+
+  const buffers = initBuffers(gl);
+
+  function render(now) {
+    drawScene(gl, programInfo, buffers, now);
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+function initBuffers(gl) {
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  return {
+    position: positionBuffer,
+  };
+}
+
+function drawScene(gl, programInfo, buffers, time) {
+  resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute.
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+  }
+
+  gl.useProgram(programInfo.program);
+
+  // Shader uniforms
+  gl.uniform1f(programInfo.uniformLocations.time, time * 0.001);
+  gl.uniform2f(programInfo.uniformLocations.resolution, gl.canvas.width, gl.canvas.height);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
+}
+
+function initShaderProgram(gl, vertexSource, fragmentSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.log("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+async function fetchShader(shader) {
+  let response = await fetch(shader);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  let content = await response.text();
+  return content;
+}
+
+function resizeCanvasToDisplaySize(canvas) {
+  const displayWidth = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  const needResize = canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+  if (needResize) {
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
+main();
---
+++ fresnel-glow/fragment.glsl
@@ -0,0 +1,83 @@
+precision highp float;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float sdSphere(vec3 point, float radius) {
+    return length(point) - radius;
+}
+
+mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+    
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+
+vec3 rotate(vec3 v, vec3 axis, float angle) {
+	mat4 m = rotationMatrix(axis, angle);
+	return (m * vec4(v, 1.0)).xyz;
+}
+
+float displacement(vec3 p) {
+    return sin(2. * p.x) * sin(2. * p.y) * sin(2. * p.z);
+}
+
+float sdf(vec3 p) {
+    vec3 p1 = rotate(p, vec3(sin(u_time), cos(u_time), 1.), u_time / 2.);
+    float sphere = sdSphere(p1, 0.5);
+    float d = displacement(p1);
+    return mix(sphere, d, .5);
+}
+
+// https://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 calcNormal(in vec3 p) {
+    const float eps = 0.0001;
+    const vec2 h = vec2(eps,0);
+    return normalize(vec3(sdf(p+h.xyy) - sdf(p-h.xyy),
+                          sdf(p+h.yxy) - sdf(p-h.yxy),
+                          sdf(p+h.yyx) - sdf(p-h.yyx)));
+}
+
+void main() {
+    vec2 uv = (2. * gl_FragCoord.xy - u_resolution.xy) / u_resolution.y;
+    vec3 camPos = vec3(0., 0., 2);
+    vec3 rayDir = normalize(vec3(uv, -1.8));
+
+    float dist = 0.;
+    float distMax = 4.;
+    vec3 currentPos;
+    float p;
+    
+    // raymarch, limit to 128 iterations
+    for(float i = 0.; i < 128.; ++i) {
+        currentPos = camPos + dist * rayDir;
+        float h = sdf(currentPos); //abs to peek inside
+
+        dist += h;
+        p = i;
+        if(h < 0.004 || dist > distMax) break;
+    }
+
+    vec3 color = vec3(.0625);
+    float fresnel;
+
+    if(dist < distMax) {
+        vec3 normal = calcNormal(currentPos);
+        float diffuse = dot(vec3(1.), normal);
+        color = vec3(diffuse);
+        color *= vec3(.2, .5, .8);
+
+        fresnel = pow(1. + dot(normal, rayDir), 3.);
+        color *= fresnel * 2.;
+    } else {
+        color = 1. - vec3(1. - ((3. * p) / 256.));
+    }
+
+    gl_FragColor = vec4(color, 1.);
+}
